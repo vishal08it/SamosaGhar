e5b49239f13a7087a59ae3b6e9642e3e5265339c
vishalraj08it <vishalraj08it@gmail.com>
2024-11-17 11:09:17 +0530

    notofication added

---
+++ SamosaGhar/Models/Notification.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace SamosaGhar.Models
+{
+    public class Notification
+    {
+        public string Type { get; set; }
+        public string Message { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string UserId { get; set; }
+    }
+
+
+
+
+}
